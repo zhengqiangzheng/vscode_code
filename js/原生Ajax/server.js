f1cf01424fe22d4118af016554ee28d8c2785e9d
zhengqiangzheng <2466877980@qq.com>
2020-01-07 08:05:05 +0800

    ajax

--- js/原生Ajax/server.js
+++ js/原生Ajax/server.js
@@ -3,9 +3,14 @@ let app = express();
 let { resolve } = require('path');
 let publicpath = resolve(__dirname, 'public');
 app.use(express.static(publicpath));
+app.use(express.urlencoded({ extended: true }));
 app.get('/test_get', (request, response) => {
   console.log(request.query);
   response.send('xiongdinihao');
+});
+app.post('/test_post', (request, response) => {
+  console.log(request.body);
+  response.send('post请求');
 });
 app.listen('3000', err => {
   if (!err) {
