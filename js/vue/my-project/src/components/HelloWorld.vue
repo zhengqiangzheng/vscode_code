4b792e95cfb6536b4907479f79dc31f12cfbdedb
zhengqiangzheng <2466877980@qq.com>
2020-02-14 09:33:13 +0800

    vue init code

---
+++ js/vue/my-project/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+    <h1>{{msg1}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      msg1: "123"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
