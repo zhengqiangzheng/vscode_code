3b8a2f7b25358908400f4c2d2edbb5ad75762130
zhengqiangzheng <2466877980@qq.com>
2020-06-25 20:09:35 +0800

    11

--- js/vue/wechat/README.md
+++ js/vue/wechat/README.md
@@ -1,28 +1,18 @@
-# my-project
+# wechat
 
 ## Project setup
 ```
-npm install
+yarn install
 ```
 
 ### Compiles and hot-reloads for development
 ```
-npm run serve
+yarn serve
 ```
 
 ### Compiles and minifies for production
 ```
-npm run build
-```
-
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
+yarn build
 ```
 
 ### Customize configuration
---
+++ js/vue/wechat/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <UserList />
+    <Chat />
+  </div>
+</template>
+
+<script>
+import Chat from "./components/Chat";
+import UserList from "./components/UserList";
+
+export default {
+  name: "App",
+  components: {
+    UserList,
+    Chat
+  },
+  data() {
+    return {
+      userList: [
+        { userName: "小明", avater: require("./assets/first.jpeg") },
+        { userName: "小红", avater: require("./assets/second.png") },
+        { userName: "小白", avater: require("./assets/3.jpeg") },
+        { userName: "小李", avater: require("./assets/4.jpeg") },
+        { userName: "小红", avater: require("./assets/5.jpeg") }，
+        { userName: "小绿", avater: require("./assets/6.jpeg") }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
